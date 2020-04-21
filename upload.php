a525c9821f6ef93913aff5d5dfc324b41a8b1d30
P1eas1ng <63578449+P1eas1ng@users.noreply.github.com>
2020-04-21 12:34:53 -0400

    Update upload.php

--- upload.php
+++ upload.php
@@ -1,1 +1,32 @@
+<?php
+// set folder to upload files
+$output_dir = "uploads/";
 
+if(isset($_FILES["myfile"]))
+{
+    $ret = array();
+
+    $error = $_FILES["myfile"]["error"];
+
+    // upload single file
+    if(!is_array($_FILES["myfile"]["name"])) //single file
+    {
+        $fileName = $_FILES["myfile"]["name"];
+        move_uploaded_file($_FILES["myfile"]["tmp_name"],$output_dir.$fileName);
+        $ret[]= $fileName;
+    }
+    else
+    {
+        // Handle Multiple files
+        $fileCount = count($_FILES["myfile"]["name"]);
+        for($i=0; $i      $fileCount; $i++)
+        {
+            $fileName = $_FILES["myfile"]["name"][$i];
+            move_uploaded_file($_FILES["myfile"]["tmp_name"][$i],$output_dir.$fileName);
+            $ret[]= $fileName;
+        }
+    }
+    // output file names as comma seperated strings to display status
+    echo json_encode($ret);
+}
+?>
