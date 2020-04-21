6c3c141c968d0e97997d2d7e62911e53c7712536
P1eas1ng <63578449+P1eas1ng@users.noreply.github.com>
2020-04-21 12:32:52 -0400

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,24 @@
-
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Upload Files</title>
+        <link href="/css/uploadfile.min.css" rel="stylesheet">
+        <script src="/js/jquery-1.10.2.js"></script>
+        <script src="/js/jquery.uploadfile.min.js"></script>
+    </head>
+    <body>
+        <div id="multiple_file_uploader">Upload</div>
+        <script>
+        $(document).ready(function() {
+            $("#multiple_file_uploader").uploadFile({
+                fileName : "myfile",
+                url : "upload.php",
+                multiple : true,
+                maxFileCount : 5,
+                allowedTypes : "jpg,png,gif",
+                showProgress : true
+            });
+        });
+        </script>
+    </body>
+</html>
